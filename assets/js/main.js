4418506a3455f7d5a6d2735a4f8ee4f032231356
your-sdnss <maxim32rp@gmail.com>
2021-04-02 19:45:12 +0300

    init 2

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.header-navbar__btn').on('click', function(){
+    $(this).toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+  });
+});
